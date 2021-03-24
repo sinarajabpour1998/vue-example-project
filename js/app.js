3e926b8a45ca251bbf85f0fd3ad84c0dc03e69c9
Sina <sina.rajabpour@outlook.com>
2021-03-25 00:57:40 +0430

    Added v-html and v-bind directives

--- js/app.js
+++ js/app.js
@@ -3,10 +3,15 @@ new Vue({
     data: {
         title: 'Hello Vue.js',
         fullName: '-',
+        aTag: '<a href="https://google.com">Google</a>',
+        aHref: 'https://yahoo.com'
     },
     methods: {
         changeFullname: function (event) {
             this.fullName = event.target.value;
+        },
+        sayHello: function () {
+            return 'Hello';
         }
     }
 })
