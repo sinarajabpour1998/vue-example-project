f87b45236e1231495fc42ed86ef238cf3f36eca4
Sina <sina.rajabpour@outlook.com>
2021-03-25 15:28:43 +0430

    Added event listening page

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@ new Vue({
         title: 'Hello Vue.js',
         fullName: '-',
         aTag: '<a href="https://google.com">Google</a>',
-        aHref: 'https://yahoo.com'
+        aHref: 'https://yahoo.com',
+        counter: 0,
+        x: 0,
+        y: 0
     },
     methods: {
         changeFullname: function (event) {
@@ -12,6 +15,27 @@ new Vue({
         },
         sayHello: function () {
             return 'Hello';
+        },
+        increaseValue: function () {
+            this.counter ++;
+        },
+        decreaseValue: function (val, event) {
+            if (this.counter != 0 && this.counter != 1){
+                this.counter -= val;
+            }else {
+                alert('Counter can not be ' + this.counter);
+            }
+            console.log(event);
+        },
+        coordinates: function (event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        },
+        stopCoordinate: function (event) {
+            event.stopPropagation();
+        },
+        validateText: function () {
+            alert('Enter pressed!');
         }
     }
 })
